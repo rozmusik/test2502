dd659d9c1458109c262de54f294d0b334bc54983
rozmusik <rozmusik@gmail.com>
2020-02-25 19:00:28 +0100

    Additional files

---
+++ Szkolenie032020/Tests.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Szkolenie032020
+{
+    public class Tests
+    {
+        ChromeDriver driver;
+
+        [Test]
+        public void GoogleSearchTest()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+
+            driver.Navigate().GoToUrl("https://www.google.com");
+
+            driver.FindElement(By.Name("q")).SendKeys("Look4Up");
+
+            Thread.Sleep(500);
+
+            driver.FindElement(By.Name("btnK")).Click();
+
+            var aaa = driver.Title;
+
+            Assert.That(aaa.Contains("Look4Up"));
+
+            driver.Quit();
+        }
+    }
+}
