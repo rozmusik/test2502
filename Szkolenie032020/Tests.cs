56474f292a6a3a12ee096f0df121665739f4817c
Bartosz Rozmus <bro@spyro-soft.com>
2020-03-05 08:13:37 +0100

    lesson2

--- Szkolenie032020/Tests.cs
+++ Szkolenie032020/Tests.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using OpenQA.Selenium.Support.UI;
 
 namespace Szkolenie032020
 {
@@ -31,6 +32,57 @@ namespace Szkolenie032020
             var aaa = driver.Title;
 
             Assert.That(aaa.Contains("Look4Up"));
+
+            driver.Quit();
+        }
+
+        [Test]
+        public void SearchByAllSelectors()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+
+            driver.Navigate().GoToUrl("https://www.phptravels.net/home");
+
+            var header = driver.FindElement(By.Id("header-waypoint-sticky"));
+
+            var arrow = driver.FindElement(By.ClassName("slick-next"));
+
+            var search = driver.FindElement(By.CssSelector("#hotels .btn-block"));
+
+            var visa = driver.FindElement(By.XPath("//*[@id=\"mobileMenuMain\"]/nav/ul[2]/li[1]/a"));
+
+            var buttonBar = driver.FindElement(By.ClassName("menu-horizontal-02"));
+            var buttons = buttonBar.FindElements(By.TagName("li"));
+            buttons[3].Click();
+
+
+            //visa.Click();
+
+            driver.Quit();
+        }
+
+        [Test]
+        public void DropDownTest()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+
+            driver.Navigate().GoToUrl("http://the-internet.herokuapp.com/dropdown");
+
+            var dropdown = driver.FindElement(By.Id("dropdown"));
+            var dropdownSelect = new SelectElement(dropdown);
+
+            var before = dropdownSelect.SelectedOption.Text;
+
+            dropdownSelect.SelectByText("Option 2");
+
+            dropdownSelect.SelectByIndex
+            dropdownSelect.SelectByValue
+
+            var after = dropdownSelect.SelectedOption.Text;
+
+            Assert.AreEqual("Option 2", dropdownSelect.SelectedOption.Text);
 
             driver.Quit();
         }
